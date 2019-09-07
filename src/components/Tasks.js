7a4e0e381e1a13d7ffa4d7def03bc7b37c306e1c
aurimas-darguzis <aurimas@darguzis.com>
2019-09-07 23:05:33 +0100

    tasks on the screen

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Checkbox } from './Checkbox';
 import { useTasks } from '../hooks';
+import { collatedTasks } from '../constants';
+import { getTitle, getCollatedTitle, collatedTasksExist } from '../helpers';
+import { useSelectedProjectValue, useProjectsValue } from '../context';
 
 export const Tasks = () => {
-  const { tasks } = useTasks('1');
-  console.log(tasks);
+  const { selectedProject } = useSelectedProjectValue();
+  const { projects } = useProjectsValue();
+  const { tasks } = useTasks(selectedProject);
+  console.log('tasks: ', tasks);
 
   let projectName = '';
+
+  if (projects && selectedProject && !collatedTasksExist(selectedProject)) {
+    projectName = getTitle(projects, selectedProject).name;
+    // console.log('project 1: ', projectName);
+  }
+
+  if (collatedTasksExist(selectedProject) && selectedProject) {
+    projectName = getCollatedTitle(collatedTasks, selectedProject).name || 'ld';
+    // console.log('project 2: ', projectName);
+  }
+
+  useEffect(() => {
+    document.title = `${projectName}: Todoist`;
+  });
 
   return (
     <div className='tasks' data-testid='tasks'>
--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -10,41 +10,66 @@ import { Projects } from '../Projects';
 import { AddProject } from '../AddProject';
 
 export const Sidebar = () => {
-  const { setSelectedProject } = useSelectedProjectValue;
+  const { setSelectedProject } = useSelectedProjectValue();
   const [active, setActive] = useState('inbox');
   const [showProjects, setshowProjects] = useState(true);
 
   return (
     <div className='sidebar' data-testid='sidebar'>
       <ul className='sidebar__generic'>
-        <li data-testid='inbox' className='inbox'>
+        <li
+          data-testid='inbox'
+          className={active === 'inbox' ? 'active' : undefined}
+          onClick={() => {
+            setActive('inbox');
+            setSelectedProject('INBOX');
+          }}
+        >
           <span>
             <FaInbox />
           </span>
           <span>Inbox</span>
         </li>
-        <li data-testid='inbox' className='inbox'>
+        <li
+          data-testid='today'
+          className={active === 'today' ? 'active' : undefined}
+          onClick={() => {
+            setActive('today');
+            setSelectedProject('TODAY');
+          }}
+        >
           <span>
             <FaRegCalendar />
           </span>
           <span>Today</span>
         </li>
-        <li data-testid='next_7' className='next_7'>
+        <li
+          data-testid='next_7'
+          className={active === 'next_7' ? 'active' : undefined}
+          onClick={() => {
+            setActive('next_7');
+            setSelectedProject('NEXT_7');
+          }}
+        >
           <span>
             <FaRegCalendarAlt />
           </span>
           <span>Next 7 days</span>
         </li>
       </ul>
-      <div className='sidebar__middle'>
+      <div
+        className='sidebar__middle'
+        onClick={() => setshowProjects(!showProjects)}
+      >
         <span>
-          <FaChevronDown />
+          <FaChevronDown
+            className={!showProjects ? 'hidden-projects' : undefined}
+          />
         </span>
         <h2>Projects</h2>
       </div>
-      <ul className='sidebar__projects'>{showProjects}</ul>
-      {/* {showProjects && <AddProject />} */}
-      <Projects />
+      <ul className='sidebar__projects'>{showProjects && <Projects />}</ul>
+      {showProjects && <AddProject />}
     </div>
   );
 };
