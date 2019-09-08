7c03ff5647724d357cffa2a432f5f226e9e881b3
aurimas-darguzis <aurimas@darguzis.com>
2019-09-08 13:19:16 +0100

    fixed layout

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -20,41 +20,65 @@ export const Sidebar = () => {
         <li
           data-testid='inbox'
           className={active === 'inbox' ? 'active' : undefined}
-          onClick={() => {
-            setActive('inbox');
-            setSelectedProject('INBOX');
-          }}
         >
-          <span>
-            <FaInbox />
-          </span>
-          <span>Inbox</span>
+          <div
+            data-testid='inbox-action'
+            onClick={() => {
+              setActive('inbox');
+              setSelectedProject('INBOX');
+            }}
+            onKeyDown={() => {
+              setActive('inbox');
+              setSelectedProject('INBOX');
+            }}
+          >
+            <span>
+              <FaInbox />
+            </span>
+            <span>Inbox</span>
+          </div>
         </li>
         <li
-          data-testid='today'
+          data-testid='today-action'
           className={active === 'today' ? 'active' : undefined}
-          onClick={() => {
-            setActive('today');
-            setSelectedProject('TODAY');
-          }}
         >
-          <span>
-            <FaRegCalendar />
-          </span>
-          <span>Today</span>
+          <div
+            data-testid='today-action'
+            onClick={() => {
+              setActive('today');
+              setSelectedProject('TODAY');
+            }}
+            onKeyDown={() => {
+              setActive('today');
+              setSelectedProject('TODAY');
+            }}
+          >
+            <span>
+              <FaRegCalendar />
+            </span>
+            <span>Today</span>
+          </div>
         </li>
         <li
           data-testid='next_7'
           className={active === 'next_7' ? 'active' : undefined}
-          onClick={() => {
-            setActive('next_7');
-            setSelectedProject('NEXT_7');
-          }}
         >
-          <span>
-            <FaRegCalendarAlt />
-          </span>
-          <span>Next 7 days</span>
+          <div
+            data-testid='next_7-action'
+            onClick={() => {
+              setActive('next_7');
+              setSelectedProject('NEXT_7');
+            }}
+            onKeyDown={() => {
+              setActive('next_7');
+              setSelectedProject('NEXT_7');
+            }}
+          >
+            <span>
+              <FaRegCalendarAlt />
+            </span>
+            <span>Next 7 days</span>
+          </div>
         </li>
       </ul>
       <div
