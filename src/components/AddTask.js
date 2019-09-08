c14e5af56783785b666d39f4932cf9bb771f560a
aurimas-darguzis <aurimas@darguzis.com>
2019-09-08 01:06:26 +0100

    feat: addTask

---
+++ src/components/AddTask.js
@@ -0,0 +1,139 @@
+import React, { useState } from 'react';
+import { FaRegListAlt, FaRegCalendarAlt } from 'react-icons/fa';
+import moment from 'moment';
+import { firebase } from '../firebase';
+import { useSelectedProjectValue } from '../context';
+
+export const AddTask = ({
+  showAddTaskMain = true,
+  shouldShowMain = false,
+  showdQuickAddTask,
+  setShowQuickAddTask,
+  showQuickAddTask
+}) => {
+  const [task, setTask] = useState('');
+  const [taskDate, setTaskDate] = useState('');
+  const [project, setProject] = useState('');
+  const [showMain, setShowMain] = useState(shouldShowMain);
+  const [showProjectOverlay, setShowProjectOverlay] = useState(false);
+  const [showTaskDate, setShowTaskDate] = useState(false);
+
+  const { selectedProject } = useSelectedProjectValue();
+
+  const addTask = () => {
+    const projectId = project || selectedProject;
+    let collateDate = '';
+
+    if (projectId === 'TODAY') {
+      collateDate = moment().format('DD/MM/YYYY');
+    } else if (projectId === 'NEXT_7') {
+      collateDate = moment()
+        .add(7, 'days')
+        .format('DD/MM/YYYY');
+    }
+    return (
+      task &&
+      projectId &&
+      firebase
+        .firestore()
+        .collection('task')
+        .add({
+          projectId,
+          task,
+          archived: false,
+          date: collateDate || taskDate,
+          userId: 'djA45kaeOP1sd'
+        })
+        .then(() => {
+          setTask('');
+          setProject('');
+          setShowMain('');
+          setShowProjectOverlay(false);
+        })
+    );
+  };
+
+  return (
+    <div
+      className={showdQuickAddTask ? 'add-task add-task__overlay' : 'add-task'}
+      data-testid='add-task-comp'
+    >
+      {showAddTaskMain && (
+        <div
+          className='add-task__shallow'
+          data-testid='show-main-action'
+          onClick={() => setShowMain(!showMain)}
+        >
+          <span className='add-task__plus'>+</span>
+          <span className='add-task__text'>Add Task</span>
+        </div>
+      )}
+
+      {(showMain || showdQuickAddTask) && (
+        <div className='add-task__main' data-testid='add-task-main'>
+          {showdQuickAddTask && (
+            <>
+              <div data-testid='quick-add-task'>
+                <h2 className='header'>Quick Add Task</h2>
+                <span
+                  className='add-task__cancel-x'
+                  data-testid='add-task-quick-cancel'
+                  onClick={() => {
+                    setShowMain(false);
+                    setShowProjectOverlay(false);
+                    setShowQuickAddTask(false);
+                  }}
+                >
+                  X
+                </span>
+              </div>
+            </>
+          )}
+          <p>Project overlay here</p>
+          <p>TaskDate here</p>
+          <input
+            className='add-task__content'
+            data-testid='add-task-content'
+            type='text'
+            value={task}
+            onChange={e => setTask(e.target.value)}
+          />
+          <button
+            type='button'
+            className='add-task__submit'
+            data-testid='add-task'
+            onClick={() => addTask()}
+          >
+            Add Task
+          </button>
+          {!showQuickAddTask && (
+            <span
+              className='add-task__cancel'
+              data-testid='add-task-main-cancel'
+              onClick={() => {
+                setShowMain(false);
+                setShowProjectOverlay(false);
+              }}
+            >
+              Cancel
+            </span>
+          )}
+          <span
+            className='add-task__project'
+            data-testid='show-project-overlay'
+            onClick={() => setShowProjectOverlay(!showProjectOverlay)}
+          >
+            <FaRegListAlt />
+          </span>
+          <span
+            className='add-task__date'
+            data-testid='show-task-date-overlay'
+            onClick={() => setShowTaskDate(!showTaskDate)}
+          >
+            <FaRegCalendarAlt />
+          </span>
+        </div>
+      )}
+    </div>
+  );
+};
