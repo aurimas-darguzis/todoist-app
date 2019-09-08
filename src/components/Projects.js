ca146bb61819bc99c7d1d99472a95a8987abbebf
aurimas-darguzis <aurimas@darguzis.com>
2019-09-08 13:23:36 +0100

    fixed layout

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -12,19 +12,27 @@ export const Projects = ({ activeValue = null }) => {
     projects.map(project => (
       <li
         key={project.projectId}
-        data-doc-id={project.docId}
         data-testid='project-action'
+        data-doc-id={project.docId}
         className={
           active === project.projectId
             ? 'active sidebar__project'
             : 'sidebar__project'
         }
-        onClick={() => {
-          setActive(project.projectId);
-          setSelectedProject(project.projectId);
-        }}
       >
-        <IndividualProject project={project} />
+        <div
+          data-testid='project-action'
+          onClick={() => {
+            setActive(project.projectId);
+            setSelectedProject(project.projectId);
+          }}
+          onKeyDown={() => {
+            setActive(project.projectId);
+            setSelectedProject(project.projectId);
+          }}
+        >
+          <IndividualProject project={project} />
+        </div>
       </li>
     ))
   );
