eaeb74ad5422536d9d33481a9764e6b82bd0cdad
aurimas-darguzis <aurimas@darguzis.com>
2019-09-15 12:03:33 +0100

    test(checkbox): 100% coverage for component

---
+++ src/__tests__/Checkbox.spec.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { render, cleanup, fireEvent } from '@testing-library/react';
+import { Checkbox } from '../components/Checkbox';
+import expectExport from 'expect';
+
+beforeEach(cleanup);
+
+jest.mock('../firebase', () => ({
+  firebase: {
+    firestore: jest.fn(() => ({
+      collection: jest.fn(() => ({
+        doc: jest.fn(() => ({
+          update: jest.fn()
+        }))
+      }))
+    }))
+  }
+}));
+
+describe('<Checkbox />', () => {
+  describe('Success', () => {
+    it('renders the task checkbox', () => {
+      const { queryByTestId } = render(<Checkbox id='1' />);
+      expectExport(queryByTestId('checkbox-action')).toBeTruthy();
+    });
+
+    it('renders the task checkbox and accepts a click', () => {
+      const { queryByTestId } = render(<Checkbox id='1' />);
+      expectExport(queryByTestId('checkbox-action')).toBeTruthy();
+      fireEvent.click(queryByTestId('checkbox-action'));
+    });
+
+    it('renders the task checkbox and accepts a onKeyDown', () => {
+      const { queryByTestId } = render(<Checkbox id='1' />);
+      expectExport(queryByTestId('checkbox-action')).toBeTruthy();
+      fireEvent.keyDown(queryByTestId('checkbox-action'));
+    });
+  });
+});
